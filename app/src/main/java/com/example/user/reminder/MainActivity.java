84feed325333ed73bc49a54b3bd0e82c889f8ed9
Jeremy <530234905@qq.com>
2015-06-24 16:50:43 +0800

    created tabs

--- app/src/main/java/com/example/user/reminder/MainActivity.java
+++ app/src/main/java/com/example/user/reminder/MainActivity.java
@@ -1,25 +1,49 @@
 package com.example.user.reminder;
 
 import android.app.Activity;
+import android.app.ActivityGroup;
+import android.app.LocalActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.TabHost;
 import android.widget.Toast;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.app.TabActivity;
 
+public class MainActivity extends Activity{
 
-public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec specs = tabHost.newTabSpec("tag1");
+        specs.setContent(R.id.tab1);
+        specs.setIndicator("first");
+        tabHost.addTab(specs);
+
+        TabHost.TabSpec specs2 = tabHost.newTabSpec("tag2");
+        specs2.setContent(R.id.tab2);
+        specs2.setIndicator("second");
+        tabHost.addTab(specs2);
+
+        TabHost.TabSpec specs3 = tabHost.newTabSpec("tag3");
+        specs3.setContent(R.id.tab3);
+        specs3.setIndicator("third");
+        tabHost.addTab(specs3);
+
     }
 
     @Override
@@ -44,7 +68,9 @@ public class MainActivity extends Activity {
         }
 
         if( id== R.id.action_edit){
-            Toast.makeText(this,"edit",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent();
+            intent.setClass(MainActivity.this, SpinnerActivity.class);
+            startActivity(intent);
             return true;
         }
 
